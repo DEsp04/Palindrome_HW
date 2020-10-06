fb53730df77000af9884abdd66a186e01b79aee3
david espinal <davidespinal@davids-MacBook-Pro.local>
2020-10-06 11:09:57 -0400

    Completed Palindrome homework

--- js/javascript.js
+++ js/javascript.js
@@ -1,3 +1,17 @@
 function palindromeCheck() {
-  let text = document.checker.palindrome.value;
+  let str = document.checker.palindrome.value;
+
+  let pElement = document.createElement("P");
+  let content = document.createTextNode(str + " is a palindrome");
+  let contentTwo = document.createTextNode(str + " is not a palindrome");
+
+  document.getElementById("ansbox").innerHTML = "";
+
+  if (str == str.split("").reverse().join("")) {
+    pElement.appendChild(content);
+    document.getElementById("ansbox").appendChild(pElement);
+  } else {
+    pElement.appendChild(contentTwo);
+    document.getElementById("ansbox").appendChild(pElement);
+  }
 }
